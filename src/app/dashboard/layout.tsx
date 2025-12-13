bfc7b5631339a997eef8f03e4b3b5b569f8cc305
xmuu <iqbalmu1601@gmail.com>
2025-12-13 19:18:14 +0700

    fix: admin layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -11,16 +11,20 @@ export default function AdminLayout({
   children: React.ReactNode
 }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 px-4">
-          <SidebarTrigger className="-ml-1 shadow ring ring-gray-200 fixed" />
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 2xl:px-32">
-          {children}
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+    <html lang="en">
+      <body className="antialiased min-h-screen flex flex-col justify-between">
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarInset>
+            <header className="flex h-16 shrink-0 items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1 shadow ring ring-gray-200 fixed" />
+            </header>
+            <div className="flex flex-1 flex-col gap-4 p-4 pb-16 2xl:px-32">
+              {children}
+            </div>
+          </SidebarInset>
+        </SidebarProvider>
+      </body>
+    </html>
   )
 }
