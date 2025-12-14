e6fa6b452b8ceea29237524b644e9870a7410b14
xmuu <iqbalmu1601@gmail.com>
2025-12-14 21:12:01 +0700

    fix: add callback url uploadthing route

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,7 +1,14 @@
 import { createRouteHandler } from "uploadthing/next"
 import { ourFileRouter } from "./core"
 
+export const runtime = 'nodejs'
+export const maxDuration = 60
+
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
+  config: {
+    callbackUrl: process.env.VERCEL_URL 
+      ? `https://${process.env.VERCEL_URL}/api/uploadthing`
+      : undefined,
+  },
 })
-
